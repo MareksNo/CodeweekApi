c02f4e2a8065fa8cac0165658976c1794be74a53
MareksNo <ulmanismareks@gmail.com>
2021-09-15 17:32:08 +0100

    Changed cors whitelist

--- CodeweekApi/settings/base.py
+++ CodeweekApi/settings/base.py
@@ -159,6 +159,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:8000",
     "http://localhost:3001",
     "http://127.0.0.1:3001",
+    "https://youthdeal.vercel.app/"
 ]
 
 # Rest framework authentication
